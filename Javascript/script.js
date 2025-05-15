0ce26591a2bbd9aab31bbef68f3c6713dd4de1dc
MADI Badrane <madibadrane405@gmail.com>
2025-05-15 13:44:51 +0200

    j'ai donner du style a ma page

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,32 +1,47 @@
 let listeSeries = document.getElementById("les_series");
+
 function afficheLesSeries() {
-    const series = ["Breaking bad", "Game of Trones", "The Walking Dead",
-                            "The Big Bang Theory", "The Office", "The Simpsons",
-                            "The Flash", "The Martian", "The X-Files", ]
-    series.forEach(serie =>{
-        //Creation de la boite de film
+    const series = [
+        "Breaking bad", "Game of Trones", "The Walking Dead",
+        "The Big Bang Theory", "The Office", "The Simpsons",
+        "The Flash", "The Martian", "The X-Files"
+    ];
+
+    series.forEach(serie => {
+        // Créer une boîte
         const boxSeries = document.createElement("div");
-        boxSeries.id = `box-${series}`;
-        boxSeries.style.border = "1ps solid black";
+        boxSeries.id = `box-${serie.replace(/\s+/g, "_")}`;
+        boxSeries.style.border = "1px solid black";
         boxSeries.style.textAlign = "center";
-        boxSeries.style.margin = "auto";
-        //Image
+        boxSeries.style.margin = "10px auto";
+        boxSeries.style.width = "200px";
+
+        // Image
         const img = document.createElement("img");
         img.src = `Images/${serie}.jpg`;
-        img.alt = `${serie}`;
+        img.alt = serie;
         img.style.width = "180px";
         img.style.height = "250px";
-        // met le titre du film
-        const titre = document.createElement("h5)");
-        titre.innerTexte = `${serie}`;
-        //description
+
+        // Titre
+        const titre = document.createElement("h5");
+        titre.innerText = serie;
+
+        // Description
         const description = document.createElement("p");
-        description.innerTexte = "le film est disponible sur tout le plateforme de streaming?";
-        //ajout des éléments dans la boite
+        description.innerText = "Le film est disponible sur toutes les plateformes de streaming.";
+
+        // Ajouter les éléments dans la boîte
         boxSeries.appendChild(img);
         boxSeries.appendChild(titre);
         boxSeries.appendChild(description);
-        //ajout de la boite dans la liste
+
+        // Ajouter la boîte à la liste
         listeSeries.appendChild(boxSeries);
     });
 }
+
+// Lancer automatiquement après le chargement de la page
+window.onload = () => {
+    afficheLesSeries();
+};
